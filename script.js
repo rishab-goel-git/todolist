bff05daf085e0ae75b83406bcbd4e8a98a2ababd
rishab-goel-git <rishu1450@gmail.com>
2020-05-21 12:27:01 +0530

    enter button handled

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ let btnAdd = $('#btnAdd')
 let btnClear = $('#btnClear')
 let inpNewTask = $('#inpNewTask')
 
-btnAdd.click(()=>{
-   let listItem = $('<li>',{
-        'class': 'list-group-item',
-       text : inpNewTask.val()
+function addItem(){
+    let listItem=$('<li>',{
+        'class' : 'list-group-item',
+        text: inpNewTask.val(),
     })
     ulTasks.append(listItem)
     listItem.click(()=>{
         listItem.toggleClass('done')
     })
+}
 
-
+inpNewTask.keypress((e)=>{
+    if(e.which==13){
+        addItem()
+    }
 })
+
+btnAdd.click(addItem)
 
 btnClear.click(()=>{
     inpNewTask.val("")
